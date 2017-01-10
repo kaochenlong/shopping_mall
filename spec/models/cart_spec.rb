442333cfd88d150c6afc4ff953f086ab04c69c7b
Eddie Kao <eddie@digik.com.tw>
2017-01-10 22:19:31 +0800

    add spec

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
+  it "可以把商品丟到到購物車裡，然後購物車裡就有東西了" do
+  end
+
+  it "如果加了相同種類的商品到購物車裡，購買項目(CartItem)並不會增加，但商品的數量會改變" do
+  end
+
+  it "商品可以放到購物車裡，也可以再拿出來" do
+  end
+
+  it "每個 Cart Item 都可以計算它自己的金額(小計)" do
+  end
+
+  it "可以計算整台購物車的總消費金額" do
+  end
+
+  it "特別活動可能可搭配折扣(例如聖誕節的時候全面打 9 折，或是滿額滿千送百)" do
+  end
 end
