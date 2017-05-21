66029317adb842caf926b70bddbaab618aa806a8
Eddie Kao <eddie@digik.com.tw>
2017-05-21 08:11:33 +0800

    add create and list features for Product

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,23 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "新增商品完成"
+    else
+      render :new
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
+  end
+end
